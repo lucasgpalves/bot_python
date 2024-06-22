d20af4131c53d6f72af459d77b52c7dacd55b564
Lucas Alves <alvez.lucas2702@gmail.com>
2024-06-22 11:43:02 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import discord
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$ping'):
+        await message.channel.send('Pong!')
+
+client.run(os.getenv('TOKEN'))
